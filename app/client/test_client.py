2e12c3ef364a9d43541e237129db621db1df7070
Egor Mikov <em@invoice.su>
2025-08-07 16:00:30 +0500

    test: enhance multithreaded test with increased worker count and task iterations

--- app/client/test_client.py
+++ app/client/test_client.py
@@ -137,16 +137,15 @@ def test_delete_queue_fail(f_auth_client):
         f_auth_client.delete_queue(2)
 
 
-@pytest.mark.skip(reason="unstable")
 def test_multithreaded(f_auth_client, f_queue_factory):
     f_queue_factory(1)
 
     def worker(runner_id: int) -> None:
-        for i in range(1):
+        for i in range(100):
             f_auth_client.add_task(1, runner_id * 100 + i, 60.0, 162030.0)
             f_auth_client.get_task(1, i)
             f_auth_client.delete_task(1, runner_id * 100 + i)
 
-    futures = [ThreadPoolExecutor(max_workers=1).submit(worker, i) for i in range(2)]
+    futures = [ThreadPoolExecutor(max_workers=20).submit(worker, i) for i in range(100)]
     for future in futures:
         future.result()
